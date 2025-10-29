4015710cb2eb57851e39ce2a29a8a4d425fd4e33
YeohTH <soundwavedecepticonleader@gmail.com>
2025-10-29 10:02:14 +0800

    fix: update vite config and add .nojekyll file for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' 
-        ? '/NUSP1-Documentation/'
-        : '/',
-    publicDir: 'public',
+    base: '/NUSP1-Documentation/',
     build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        rollupOptions: {
-            input: {
-                main: '/index.html',
-            }
-        }
+        outDir: 'docs',
+        emptyOutDir: false
     }
 })
